e08b8d7c1965a891be20bec5dcb98ab918c47c83
mafiouser <118977648+mafiouser@users.noreply.github.com>
2023-11-24 19:26:55 +0300

    Add files via upload

---
+++ Lab4/Lab4.Web.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using ShopApi.Contracts;
+using ShopApi.Entities;
+using ShopApi.LoggerService;
+using ShopApi.Repository;
+
+namespace ShopApi.Web.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+    
+    public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+
+        });
+    
+    public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddScoped<ILoggerManager, LoggerManager>();
+    
+    public static void ConfigureSqlContext(this IServiceCollection services,
+        IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b =>
+                b.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName)));
+    
+    public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+    
+    public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+        builder.AddMvcOptions(config => config.OutputFormatters.Add(new
+            CsvOutputFormatter()));
+}
---
+++ Lab4/Lab4.Web.Api/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using ShopApi.Contracts;
+using ShopApi.Web.Api.Extensions;
+
+namespace ShopApi.Web.Api;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+            "/nlog.config"));
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(Startup));
+        services.ConfigureCors();
+        services.ConfigureIISIntegration();
+        services.ConfigureLoggerService();
+        services.ConfigureSqlContext(Configuration);
+        services.ConfigureRepositoryManager();
+        services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            })
+            .AddXmlDataContractSerializerFormatters()
+            .AddCustomCSVFormatter();
+    }
+    
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHsts();
+        }
+        app.ConfigureExceptionHandler(logger);
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseCors("CorsPolicy");
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.All
+        });
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
